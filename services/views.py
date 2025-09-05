fe665418b5e7de0eab45030c1c1b0fa399abae9f
liambaker93 <liam_baker@live.com>
2025-09-05 11:56:01 +0100

    create services html template and link it with the view

--- services/views.py
+++ services/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def services(request):
     """
     A list of the services offered.
     """
-    return render(request, 'templates/services.html')
+    return render(request, 'services/services.html')
