a965719fd16693db5d328cda42f6ea1f86c14bea
liambaker93 <liam_baker@live.com>
2025-09-29 14:39:40 +0100

    update staticfiles link in settings

---
+++ appointments/static/js/calendar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          hiddenDays: [0],
+        });
+        calendar.render();
+      });
--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def appointmentsPage(request):
 
     return render(request, 'appointments/appointments.html')
+
+
