60d8a789491babb7d78c07e2000f8be19cf97fd6
liambaker93 <liam_baker@live.com>
2025-10-01 09:21:52 +0100

    create appointments model for booking

--- appointments/models.py
+++ appointments/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from services.models import ServicesList
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-## Needs migrating when finished, pls delete when migrated 
 
 class Appointments(models.Model):
 
-    name = models.ForeignKey(ServicesList, on_delete=models.PROTECT, related_name='appointments')
-    appointment_time = models.DateTimeField()
-    
-    
+    service = models.ForeignKey(ServicesList, on_delete=models.CASCADE, related_name='appointments')
+    appointment_time = models.TimeField()
+    appointment_date = models.DateField()
+    booking_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=96, blank=True, null=True)
+ 
