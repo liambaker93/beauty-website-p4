e3da85abd227b17a7e66077af43d7960a2740725
liambaker93 <liam_baker@live.com>
2025-10-13 18:33:30 +0100

    add login link to account page, add logic to handle if the user is not logged in to then view the account page

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from appointments.models import Appointments
 
 # Create your views here.
 
 def index(request):
+    """
+    This view generates the orders made by a user ready to display on the account page.
+    """
+    template = 'useraccount/account.html'
+    if request.user.is_authenticated:
+        user_appointments = Appointments.objects.filter(
+            user=request.user
+        ).select_related('service').order_by('appointment_date', 'appointment_time')
 
-    return render(request, 'useraccount/account.html')
+        context = {
+            'appointments': user_appointments,
+        }
+        return render(request, template, context)
+    else:
+        return render(request, template)
