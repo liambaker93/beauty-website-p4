d9d0cb48fab786f51a0ea953986972255a0a5095
liambaker93 <liam_baker@live.com>
2025-10-26 23:33:11 +0000

    adjust addNewService view for the error messages to be back to messages.error now that the success toast message has been tested.

--- services/views.py
+++ services/views.py
@@ -71,7 +71,7 @@ def addNewService(request):
     """
     # add blank space validation
     if not request.user.is_staff:
-        messages.success(request, "Sorry, only the store owner can do that!")
+        messages.error(request, "Sorry, only the store owner can do that!")
         return redirect(reverse('services'))
 
     if request.method == 'POST':
@@ -81,7 +81,7 @@ def addNewService(request):
             messages.success(request, "Successfully added service!")
             return redirect(reverse('services'))
         else:
-            messages.success
+            messages.error
             (request, "Adding service failed. "
                 "Please ensure the form is valid.")
     else:
