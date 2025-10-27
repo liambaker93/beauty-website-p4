b0143deef9721a3cadfda25b2da81f841c16f5c2
liambaker93 <liam_baker@live.com>
2025-10-27 22:17:48 +0000

    change debug settings for heroku

--- beauty_website/settings.py
+++ beauty_website/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://beauty-website-p4-717b16fd7429.herokuapp.com/',
                  '.127.0.0.1',
